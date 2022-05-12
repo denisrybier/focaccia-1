5fb6cd51e158fbeff504f7bc992d5a41479ef911
denisrybier <rybier97@gmail.com>
2022-05-12 01:59:32 -0300

    mcr: focaccia

---
+++ app/Http/Controllers/FocacciaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\focaccia;
+use Illuminate\Http\Request;
+
+class FocacciaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\focaccia  $focaccia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(focaccia $focaccia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\focaccia  $focaccia
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(focaccia $focaccia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\focaccia  $focaccia
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, focaccia $focaccia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\focaccia  $focaccia
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(focaccia $focaccia)
+    {
+        //
+    }
+}
