924e5e3c4f0da52da71433dc6d9d87d4e5f2d019
denisrybier <rybier97@gmail.com>
2022-05-13 00:50:44 -0300

    agregado del archivo index.blade.php para mostrar resultados

---
+++ resources/views/focaccia/index.blade.php
@@ -0,0 +1,44 @@
+
+@if(Session::has('mensaje'))
+{{Session::get('mensaje')}}
+@endif
+
+
+<table class="table table-bordered table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Descripcion</th>
+      <th scope="col">Precio</th>
+      <th scope="col">Foto</th>
+      <th scope="col">Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($focaccias as $focaccia)
+    <tr>
+      <td>{{$focaccia->id}}</td>
+      <td>{{$focaccia->Nombre}}</td>
+      <td>{{$focaccia->Descripcion}}</td>
+      <td>{{$focaccia->Precio}}</td>
+      <td>
+      <img src="{{asset('storage').'/'.$focaccia->Foto}}" alt="" width="100">
+      </td>
+      <td>
+         <a href="{{url('/focaccia/'.$focaccia->id.'/edit')}}">
+             Editar</a>
+     
+     <form action="{{url('/focaccia/'.$focaccia->id)}}" method="post">
+         @csrf
+         {{method_field('DELETE')}}
+         <input type="submit"
+          onclick="return confirm('¿Estas seguro de eliminar este usuario')"
+          value="Borrar">
+         
+     </form>
+     </td>
+    </tr>
+    
+  </tbody>
+</table>
